b566cc5091f52ae217975d95d4bf18482dc92f57
Alshoja <alshoja@gmail.com>
2021-04-02 15:38:01 +0530

    removed forwar error

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,20 +2,19 @@
 
 namespace App\Http\Controllers;
 
-use App\Events\AddTimeLineNote;
-use App\Events\RejectDocument;
-use App\Events\SignDocument;
-use App\Events\SignOrRejectProfile;
-use App\Models\Profile;
 use Exception;
+use Validator;
+use App\Models\Profile;
 use App\Models\TimeLine;
+use App\Events\SignDocument;
 use App\Models\TrackProfile;
 use Illuminate\Http\Request;
-use Validator;
+use App\Events\RejectDocument;
+use App\Events\AddTimeLineNote;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Support\Facades\DB;
 
 class ProfileController extends Controller
 {
@@ -80,105 +79,99 @@ class ProfileController extends Controller
     public function store(Request $request)
     {
         $profile = new Profile();
-            $profile->name = $request->name;
-            $profile->nationality = $request->nationality;
-            $profile->dob = $request->dob;
-            $profile->gender = $request->gender;
-            $profile->citizen_status = $request->citizen_status;
-            $profile->citizen_location = $request->citizen_location;
-            $profile->citizen_id = $request->citizen_id;
-            $profile->citizen_uid = $request->citizen_uid;
-            $profile->passport_no = $request->passport_no;
-            $profile->passport_type = $request->passport_no;
-            $profile->entered_by="";
-            $profile-> bought_by="";
-            $profile->entity="";
-            $profile->entry_date="";
-            $profile->entity_location="";
-            $profile->shipping_no="";
-            $profile->coming_from="";
-            $profile->going_to="";
-            $profile->final_destination="";
-            $profile->profile_image="";
-            $profile->product_image="";
-            $profile->doc_image="";
-            $profile->record_status="";
-            $profile->record_dep_transfer="";
-            $profile->note="";
-            $profile->belongs_to=1;
-            $profile->dep_id=1;
-            $profile->section_id=1;
-            $profile->save();
-            return response()->json(['id' => $profile->id]);
+        $profile->name = $request->name;
+        $profile->nationality = $request->nationality;
+        $profile->dob = $request->dob;
+        $profile->gender = $request->gender;
+        $profile->citizen_status = $request->citizen_status;
+        $profile->citizen_location = $request->citizen_location;
+        $profile->citizen_id = $request->citizen_id;
+        $profile->citizen_uid = $request->citizen_uid;
+        $profile->passport_no = $request->passport_no;
+        $profile->passport_type = $request->passport_no;
+        $profile->entered_by = "";
+        $profile->bought_by = "";
+        $profile->entity = "";
+        $profile->entry_date = "";
+        $profile->entity_location = "";
+        $profile->shipping_no = "";
+        $profile->coming_from = "";
+        $profile->going_to = "";
+        $profile->final_destination = "";
+        $profile->profile_image = "";
+        $profile->product_image = "";
+        $profile->doc_image = "";
+        $profile->record_status = "";
+        $profile->record_dep_transfer = "";
+        $profile->note = "";
+        $profile->belongs_to = 1;
+        $profile->dep_id = 1;
+        $profile->section_id = 1;
+        $profile->save();
+        return response()->json(['id' => $profile->id]);
     }
-    public function updateUser(Request $request){
-        if($request->ajax())
-        {
-        $entered_by=$request->input('entered_by');
-        $bought_by=$request->input('bought_by');
-        $entity=$request->input('entity');
-        $entry_date=$request->input('entry_date');
-        $entity_location=$request->input('entity_location');
-        $editid=$request->input('editid');
-        try{
-            $data = array("entered_by"=>$entered_by,"bought_by"=>$bought_by,"entity"=>$entity,"entry_date"=>$entry_date,"entity_location"=>$entity_location);
-            Profile::updateData($editid, $data);
-            return response()->json(['success'=>'Form is successfully submitted!']);
+    public function updateUser(Request $request)
+    {
+        if ($request->ajax()) {
+            $entered_by = $request->input('entered_by');
+            $bought_by = $request->input('bought_by');
+            $entity = $request->input('entity');
+            $entry_date = $request->input('entry_date');
+            $entity_location = $request->input('entity_location');
+            $editid = $request->input('editid');
+            try {
+                $data = array("entered_by" => $entered_by, "bought_by" => $bought_by, "entity" => $entity, "entry_date" => $entry_date, "entity_location" => $entity_location);
+                Profile::updateData($editid, $data);
+                return response()->json(['success' => 'Form is successfully submitted!']);
+            } catch (\Illuminate\Database\QueryException $ex) {
+                dd($ex->getMessage());
             }
-            catch(\Illuminate\Database\QueryException $ex){ 
-                dd($ex->getMessage()); 
-            }
-        
         }
     }
     public function stageThree(Request $request)
     {
-        $shipping_no=$request->input('shipping_no');
-        $coming_from=$request->input('coming_from');
-        $going_to=$request->input('going_to');
-        $final_destination=$request->input('final_destination');
-        $profile_image=$request->file('profile_image')->store('images');
-        $product_image=$request->file('product_image')->store('images');
-        $doc_image=$request->file('doc_image')->store('images');
-        $note=$request->input('note');
-        $editid1=$request->input('editid1');
-        $data = array("shipping_no"=>$shipping_no,"coming_from"=>$coming_from,"going_to"=>$going_to,"final_destination"=>$final_destination,"profile_image"=>$profile_image,"product_image"=>$product_image,"doc_image"=>$doc_image,"note"=>$note);
-       try{
-             DB::table('profiles')->where('id', $editid1)->update($data);
-             return response()->json(['success'=>'Form is successfully submitted!']);
-          }
-        catch(\Illuminate\Database\QueryException $ex){ 
-            dd($ex->getMessage()); 
+        $shipping_no = $request->input('shipping_no');
+        $coming_from = $request->input('coming_from');
+        $going_to = $request->input('going_to');
+        $final_destination = $request->input('final_destination');
+        $profile_image = $request->file('profile_image')->store('images');
+        $product_image = $request->file('product_image')->store('images');
+        $doc_image = $request->file('doc_image')->store('images');
+        $note = $request->input('note');
+        $editid1 = $request->input('editid1');
+        $data = array("shipping_no" => $shipping_no, "coming_from" => $coming_from, "going_to" => $going_to, "final_destination" => $final_destination, "profile_image" => $profile_image, "product_image" => $product_image, "doc_image" => $doc_image, "note" => $note);
+        try {
+            DB::table('profiles')->where('id', $editid1)->update($data);
+            return response()->json(['success' => 'Form is successfully submitted!']);
+        } catch (\Illuminate\Database\QueryException $ex) {
+            dd($ex->getMessage());
         }
     }
     public function stageFour(Request $request)
     {
-      $record_status=$request->input('record_status');
-      $record_dep_transfer=$request->input('record_dep_transfer');
-      $editid3=$request->input('editid3');
-      $data = array("record_status"=>$record_status,"record_dep_transfer"=>$record_dep_transfer);
-      try{
-          DB::table('profiles')->where('id', $editid3)->update($data);
-          return response()->json(['success'=>'Form is successfully submitted!']);
-        }
-     catch(\Illuminate\Database\QueryException $ex){ 
-               dd($ex->getMessage()); 
+        $record_status = $request->input('record_status');
+        $record_dep_transfer = $request->input('record_dep_transfer');
+        $editid3 = $request->input('editid3');
+        $data = array("record_status" => $record_status, "record_dep_transfer" => $record_dep_transfer);
+        try {
+            DB::table('profiles')->where('id', $editid3)->update($data);
+            return response()->json(['success' => 'Form is successfully submitted!']);
+        } catch (\Illuminate\Database\QueryException $ex) {
+            dd($ex->getMessage());
         }
     }
     public function stageFive(Request $request)
-      {
-        $belongs_to =$request->input('belongs_to');
-        $editid4=$request->input('editid4');
-        $data = array("belongs_to"=>$belongs_to);
-        try{
+    {
+        $belongs_to = $request->input('belongs_to');
+        $editid4 = $request->input('editid4');
+        $data = array("belongs_to" => $belongs_to);
+        try {
             DB::table('profiles')->where('id', $editid4)->update($data);
-            return response()->json(['success'=>'Form is successfully submitted!']);
-          }
-             catch(\Illuminate\Database\QueryException $ex){ 
-                 dd($ex->getMessage()); 
-             }
-
-      }
+            return response()->json(['success' => 'Form is successfully submitted!']);
+        } catch (\Illuminate\Database\QueryException $ex) {
+            dd($ex->getMessage());
+        }
+    }
 
     /**
      * Display the specified resource.
@@ -231,20 +224,8 @@ class ProfileController extends Controller
     public function forwardAsNew($id)
     {
         $existingProfile = Profile::find($id);
-
-        //Clone a attribute of existing row into a new copy
         $duplicateProfile = $existingProfile->replicate();
-
-        //Change a name of the copy (duplicate) Profile by appending id of the existing Profile to make the name of duplicate Profile unique
-        $duplicateProfile->general_director_id = null;
-        $duplicateProfile->director_id = null;
-        $duplicateProfile->depart_head_id = null;
-        $duplicateProfile->supervisor_id = null;
-        $duplicateProfile->is_completed = null;
         $duplicateProfile->is_drafted = null;
-        $duplicateProfile->is_notify = null;
-
-        //Now Create a duplicate Profile
         $duplicateProfile->save();
         return back()->with('message', 'Forwaded updated');
     }
@@ -299,7 +280,6 @@ class ProfileController extends Controller
         $timeLine->profile_id = $request->profile_id;
         $timeLine->note = $request->note;
 
-        // stashed content check
         $signDocEvent = SignDocument::dispatch($trackProfile);
         $addEntryEvent = AddTimeLineNote::dispatch($timeLine);
         return response()->json([$signDocEvent, $addEntryEvent], 200);
